93e04ab734d4bb5ed9d5b7f3022a9f61a1ea0520
ian.vinicius <ian.vinicius@tascominformatica.com.br>
2022-09-05 22:52:32 -0300

    initializing application in react with typescript

---
+++ src/config/routes.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as RouterDOM from "react-router-dom";
+
+const routes: RouterDOM.RouteObject[] = [
+  {
+    path: "/",
+    element: <>Menino moreno</>,
+  },
+];
+
+export default routes;
