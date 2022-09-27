bd790ad02c83e59016394a09635d010a8e7e9087
i4n-v <ianvinicioss@gmail.com>
2022-09-27 14:04:58 -0300

    General configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./assets/styles/global.css";
+import "./assets/styles/colors.css";
+import "./assets/styles/typographies.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,38 +1,52 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import styled from "styled-components";
-import { LinkButton } from "../../components/Buttons";
-import IntroductionImage from "../../assets/img/photos/introduction.jpg";
-import { LargeText, MediumText, Dot } from "../../components/Typographies";
+import { BikeCard, InformationCard } from "../../components/Cards";
+import { LinkButton, UnderlineButton } from "../../components/Buttons";
+import { NavLink } from "react-router-dom";
+import introductionImage from "../../assets/img/photos/introduction.jpg";
+import tecnologiesImage from "../../assets/img/photos/tecnologies.jpg";
+import magicHomeImage from "../../assets/img/bikes/magic-home.jpg";
+import nimbusHomeImage from "../../assets/img/bikes/nimbus-home.jpg";
+import nebulaHomeImage from "../../assets/img/bikes/nebula-home.jpg";
+import { ReactComponent as Eletrical } from "../../assets/svg/icons/eletrical.svg";
+import { ReactComponent as Searcher } from "../../assets/svg/icons/searcher.svg";
 
 const Introduction = styled.main`
   background-color: var(--c12);
   color: var(--w);
   box-shadow: inset 0 -120px var(--w);
 
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0px 40px;
+  }
+
+  > div img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
   @media (max-width: 800px) {
     background: var(--c11);
     padding-top: 40px;
     box-shadow: inset 0 -60px var(--w);
 
+    > div {
+      grid-template-columns: 1fr;
+      gap: 32px;
+    }
+
+    > div img {
+      height: 300px;
+      width: 100%;
+    }
   }
 `;
 
-const Container = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 0px 40px;
-  max-width: 1200px;
-  padding: 0px 20px;
-  margin: 0px auto;
-
-  @media (max-width: 800px) {
-    grid-template-columns: 1fr;
-    gap: 32px;
-  }
-`;
-
-const Content = styled.div`
+const IntroductionContent = styled.div`
   align-self: end;
   padding-bottom: 200px;
 
@@ -41,7 +55,7 @@ const Content = styled.div`
   }
 `;
 
-const Title = styled(LargeText)`
+const Title = styled.h1`
   margin-bottom: 32px;
 
   @media (max-width: 800px) {
@@ -49,18 +63,118 @@ const Title = styled(LargeText)`
   }
 `;
 
-const Description = styled(MediumText)`
+const Description = styled.p`
   margin-bottom: 20px;
   color: var(--c05);
 `;
 
-const Image = styled.img`
-  height: 100%;
-  object-fit: cover;
+const BikeList = styled.article`
+  padding: 60px 0px 120px 0px;
+
+  > h2 {
+    margin: 0px auto 40px auto; 
+  }
+
+  > ul {
+    display: flex;
+    gap: 40px;
+    padding: 0px 20px 20px 20px;
+    max-width: 1400px;
+    margin: 0 auto;
+    overflow-x: auto;
+  }
+
+  > ul > li {
+    flex: 1;
+    min-width: 280px;
+  }
+
+  > a {
+    display: block;
+  }
 
   @media (max-width: 800px) {
-    height: 300px;
+    padding: 60px 0px 60px 0px;
+
+    > ul {
+      gap: 20px;
+
+    }
+  }
+`;
+
+const Tecnology = styled.article`
+  background: var(--c11);
+  box-shadow: inset 0 80px var(--w), inset 0 -80px var(--w);
+
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 40px;
+  }
+
+  > div img {
     width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position : left;
+  }
+
+  @media (max-width: 800px) {
+    box-shadow: initial;
+
+    > div {
+      grid-template-columns: 1fr;      
+    }
+
+    > div img {
+      display: none;
+    }
+  }
+`;
+
+const TecnologyContent = styled.div`
+  padding: 160px 0px;
+
+  > h2 {
+    color: var(--w);
+    margin-bottom: 32px;
+  }
+
+  > span, & > p {
+    color: var(--c05);
+  }
+
+  > span {
+    display: block;
+    margin-bottom: 20px;
+  }
+
+  > p {
+    margin-bottom: 20px;
+  }
+
+  > a {
+    margin-bottom: 80px;
+  }
+
+  > div {
+    display: flex;
+    gap: 40px;    
+  }
+
+  @media (max-width: 800px) {
+    padding: 60px 0px;
+
+    > a {
+      margin-bottom: 60px;
+    }
+  }
+
+  @media (max-width: 600px) {
+    > div {
+      flex-direction: column;
+    }
   }
 `;
 
@@ -72,22 +186,78 @@ const Home = () => {
       </Helmet>
 
       <Introduction>
-        <Container>
-          <Content>
-            <Title>
-              Bicicletas feitas sob medida<Dot>.</Dot>
+        <div className="container">
+          <IntroductionContent>
+            <Title className="poppins-xxl">
+              Bicicletas feitas sob medida<span className="dot">.</span>
             </Title>
-            <Description>
+            <Description className="roboto-l">
               Bicicletas elétricas de alta precisão e qualidade, feitas sob medida para o cliente. Explore o mundo na
               sua velocidade com a Bikcraft.
             </Description>
             <LinkButton to="/bicicletas">Escolha a sua</LinkButton>
-          </Content>
+          </IntroductionContent>
           <div>
-            <Image src={IntroductionImage} alt="Bicicleta elétrica preta." />
+            <img src={introductionImage} alt="Bicicleta elétrica preta." />
           </div>
-        </Container>
+        </div>
       </Introduction>
+
+      <BikeList>
+        <h2 className="poppins-xxl container">
+          escolha a sua<span className="dot">.</span>
+        </h2>
+
+        <ul>
+          <li>
+            <NavLink to="/bicicletas/magic_might">
+              <BikeCard title="Magic Might" price={2499} image={{ src: magicHomeImage, alt: "Bicicleta preta" }} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/bicicletas/nimbus_stark">
+              <BikeCard title="Nimbus Stark" price={4999} image={{ src: nimbusHomeImage, alt: "Bicicleta preta" }} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/bicicletas/nebula_cosmic">
+              <BikeCard title="Nebula Cosmic" price={3999} image={{ src: nebulaHomeImage, alt: "Bicicleta preta" }} />
+            </NavLink>
+          </li>
+        </ul>
+      </BikeList>
+
+      <Tecnology>
+        <div className="container">
+          <TecnologyContent>
+            <span className="roboto-l-b">Tecnologia avançada</span>
+            <h2 className="poppins-xxl">
+              você escolhe as suas cores e componentes<span className="dot">.</span>
+            </h2>
+            <p className="roboto-l">
+              Cada Bikcraft é única e possui a sua identidade. As medidas serão exatas para o seu corpo e altura,
+              garantindo maior conforto e ergonomia na sua pedalada. Você pode também personalizar completamente as suas
+              cores.
+            </p>
+            <UnderlineButton to="/bicicletas">ESCOLHA UM MODELO</UnderlineButton>
+            <div>
+              <InformationCard
+                icon={<Eletrical />}
+                title="Motor Elétrico"
+                description="Toda Bikcraft é equipada com um motor elétrico que possui duração de até 120h. A bateria é recarregada com a sua energia gasta ao pedalar."
+              />
+              <InformationCard
+                icon={<Searcher />}
+                title="Rastreador"
+                description="Sabemos o quão preciosa é a sua Bikcraft, por isso adicionamos rastreadores e sistemas anti-furto para garantir o seu sossego."
+              />
+            </div>
+          </TecnologyContent>
+          <div>
+            <img src={tecnologiesImage} alt="tecnologias" />
+          </div>
+        </div>
+      </Tecnology>
     </>
   );
 };
