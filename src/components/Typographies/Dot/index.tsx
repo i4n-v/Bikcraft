0b16b2e4a857907b526cb58c56841316c6ed93e8
i4n-v <ianvinicioss@gmail.com>
2022-09-27 14:03:24 -0300

    Removing typographies components

--- src/components/Typographies/Dot/index.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Dot = styled.span`
-  color: #FFBB00;
-  font-family: inherit;
-`;
-
-export default Dot;
--- src/components/Typographies/index.tsx
+++
@@ -1,5 +0,0 @@
-import Dot from "./Dot";
-import LargeText from "./LargeText";
-import MediumText from "./MediumText";
-
-export { LargeText, MediumText, Dot };
